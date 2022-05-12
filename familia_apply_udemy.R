9f86656d1b83880bbd9699e7b8ce5dd6f7c5aa6b
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-05-12 18:32:47 -0300

    Add files via upload

--- familia_apply_udemy.R
+++ familia_apply_udemy.R
@@ -12,3 +12,12 @@ dados2 <- data.frame("col1" = 1:5,
                      "col2" = c(5, 6, 9, 10, 20))
 dados2
 
+# Função apply -----------------------------------------------------------------------------------------------------------------------------
+
+### Aplica uma função a linhas ou colunas de um data.frame ou matriz
+### apply(x, margem, função, outros argumentos) 
+### A margem indica se é coluna ou linha: 1 = linha, 2 = coluna
+
+apply(dados2, 2, sum) # Soma das colunas do dataframe (cálculo feito para cada coluna)
+apply(dados2, 1, sum) # Soma das linhas do dataframe
+apply(dados2, 2, mean) # Média das colunas do dataframe
