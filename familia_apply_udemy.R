a9321f9f11d9f080e5077fd278fa17405a180846
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-05-12 18:43:41 -0300

    Add files via upload

--- familia_apply_udemy.R
+++ familia_apply_udemy.R
@@ -21,3 +21,23 @@ dados2
 apply(dados2, 2, sum) # Soma das colunas do dataframe (cálculo feito para cada coluna)
 apply(dados2, 1, sum) # Soma das linhas do dataframe
 apply(dados2, 2, mean) # Média das colunas do dataframe
+
+# Função tapply ----------------------------------------------------------------------------------------------------------------------------
+
+### Aplica uma função a um subconjunto do dataframe
+
+### Criando um dataframe com subconjuntos
+
+dados3 <- data.frame("col1" = 1:5,
+                     "col2" = c(5, 6, 9, 10, 20),
+                     "col3" = c("a", "b", "b", "a", "a"))
+dados3
+
+tapply(dados3$col1, dados3$col3, mean) # Média da coluna um em relação ao subconjunto da 
+# coluna 3
+tapply(dados3$col2, dados3$col3, mean) # Média da coluna dois em relação ao subconjunto da 
+# coluna 3
+tapply(dados3$col1, dados3$col3, sum) # Soma da coluna um em relação ao subconjunto da 
+# coluna 3
+tapply(dados3$col2, dados3$col3, sum) # Soma da coluna dois em relação ao subconjunto da 
+# coluna 3
